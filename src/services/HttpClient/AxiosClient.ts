74c81cb97ea56fd827e2c2e50df4a8f2b331a8ff
Rafael Garcia <rafael.garcia@dtidigital.com.br>
2023-04-28 15:46:23 -0300

    chore: config axios headers

--- src/services/HttpClient/AxiosClient.ts
+++ src/services/HttpClient/AxiosClient.ts
@@ -6,7 +6,13 @@ abstract class AxiosClient implements IHttpAdapter {
   public http: AxiosInstance;
 
   constructor() {
-    this.http = axios.create();
+    this.http = axios.create({
+      headers: {
+        Authorization:
+          'Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNmJmZWVmMDhkOTBhNjE3MGE5NmVkMzIxYjkxZGI0MSIsInN1YiI6IjY0NDgzZGZmNDk3NTYwMDJmY2RlZGI0ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5J495TK0UumBqY0SrhGWjSBuTxFjiYrl5ew7CSVFbI8',
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+    });
   }
 
   async get<T = object, R = AxiosResponse<T>>(
